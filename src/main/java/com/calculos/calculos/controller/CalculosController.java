18e94c37bb7850ae97315ef8a28c09f1ce065fad
VictorFerreira999 <victorferreira9990@gmail.com>
2024-11-01 09:55:07 -0300

    testante

--- src/main/java/com/calculos/calculos/controller/CalculosController.java
+++ src/main/java/com/calculos/calculos/controller/CalculosController.java
@@ -7,10 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/calculos")
@@ -29,5 +26,11 @@ public class CalculosController {
             return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
         }
     }
+
+    @PostMapping("/test")
+    public ResponseEntity<String> test() {
+        return new ResponseEntity<>("Endpoint de teste funcionando", HttpStatus.OK);
+    }
+
 
 }
